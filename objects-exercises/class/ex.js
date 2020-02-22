641711b9842bb4a1d7705e88481aaab34faa5433
George <admin@georgesimos.com>
2020-02-22 11:03:51 +0200

    ...

--- objects-exercises/class/ex.js
+++ objects-exercises/class/ex.js
@@ -1,7 +1,4 @@
 class Helpers {
-  getStudentId(record) {
-    return record.id;
-  }
   sortByNameAsc(record1, record2) {
     if (record1.name < record2.name) return -1;
     else if (record1.name > record2.name) return 1;
@@ -72,6 +69,9 @@ class Workshop extends Helpers {
     var unpaidIds = recordIds.filter(this.notYetPaid.bind(this));
 
     this.printRecords(unpaidIds);
+  }
+  getStudentId(record) {
+    return record.id;
   }
   notYetPaid(studentId) {
     var record = this.getStudentFromId(studentId);
